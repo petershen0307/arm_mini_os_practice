96c4b9668398e115ef9168bd403703789d884743
peter_shen <petershen0307@hotmail.com>
2016-07-17 16:46:30 +0800

    split code 3-4 3-9 to init.s, code 3-3 leave in start.s

--- ch3/start.s
+++ ch3/start.s
@@ -1,4 +1,3 @@
-;3-3
 .section .startup
 .code 32
 .align 0
@@ -32,48 +31,3 @@ _vector_data_abort:     .word __vector_data_abort
 _vector_reserved:       .word __vector_reserved
 _vector_irq:            .word __vector_irq
 _vector_fiq:            .word __vector_fiq
-;3-4
-.text
-.code 32
-.global __vector_reset
-
-.extern plat_boot
-.extern __bss_start__
-.extern __bss_end__
-
-__vector_reset:
-    msr cpsr_c, #(DISABLE_IRQ | DISABLE_FIQ | SVC_MOD)
-    LDR SP, =_SVC_STACK
-
-    msr cpsr_c, #(DISABLE_IRQ | DISABLE_FIQ | IRQ_MOD)
-    LDR SP, =_IRQ_STACK
-
-    msr cpsr_c, #(DISABLE_IRQ | DISABLE_FIQ | FIQ_MOD)
-    LDR SP, =_FIQ_STACK
-
-    msr cpsr_c, #(DISABLE_IRQ | DISABLE_FIQ | ABT_MOD)
-    LDR SP, =_ABT_STACK
-
-    msr cpsr_c, #(DISABLE_IRQ | DISABLE_FIQ | UND_MOD)
-    LDR SP, =_UND_STACK
-
-    msr cpsr_c, #(DISABLE_IRQ | DISABLE_FIQ | SYS_MOD)
-    LDR SP, =_SYS_STACK
-
-_clear_bss:
-    ldr r1, _bss_start_
-    ldr r3, _bss_end_
-    mov r2, #0x0
-
-l:
-    cmp r1, r3
-    beq _main
-    str r2, [r1], #0x4
-
-_main:
-    b plat_boot
-
-_bss_start_: .word __bss_start__
-_bss_end_: .word __bss_end__
-
-.end
